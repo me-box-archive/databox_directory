d762ad50c25ad99703195577ad16d3ff444824f1
Toshbrown <toshbrown@gmail.com>
2016-10-18 12:42:16 +0100

    dump directory data to / to help with debuging

--- app/app.js
+++ app/app.js
@@ -22,6 +22,7 @@ app.set('view engine', 'jade');
 
 // app setup
 app.use(logger('dev'));
+app.use(express.static(__dirname + '/static'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
